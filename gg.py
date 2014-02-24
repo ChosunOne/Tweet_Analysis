f1c59a70454f49cf21497c19db6fece0130f6880
Salil Gupta <salil.gupta323@gmail.com>
2014-02-23 22:59:22 -0600

    Final project file for Golden Globes Tweet Analysis. Finds Winners, Nominees, Presenters, Hosts, and worst dressed and best dressed

--- gg.py
+++ gg.py
@@ -1,11 +1,11 @@
 import json
 import nltk
-from nltk.tag import pos_tag
 import operator
 import re
 import collections, difflib
 import pickle
 from TweetLibrary import *
+import time
 from collections import OrderedDict
 
 gg = ['Golden Globes', 'GoldenGlobes', 'golden globes']
@@ -19,7 +19,6 @@ def loadJSONFromFile(filePath):
 		for jsonline in f:
 			json_data.append(json.loads(jsonline))
 	return json_data
-
 
 def getCategoriesFromFile(filePath):
 	awardCategories = []
@@ -47,87 +46,6 @@ def findHostTweets(text):
 		hostMentioned = True
 
 	return hostMentioned
-
-def extractProperNouns(tokenizedText):
-	# taggedText = pos_tag(tokenizedText)
-	taggedText = tagger.tag(tokenizedText)
-	grammar = "NP: {<NNP>*}"
-	cp = nltk.RegexpParser(grammar)
-	chunkedText = cp.parse(taggedText)
-
-	properNouns = []
-	for n in chunkedText:
-		if isinstance(n, nltk.tree.Tree):               
-			if n.label() == 'NP':
-				phrase = ""
-				for word, pos in n.leaves():
-					if len(phrase) == 0:
-						phrase = word
-					else:
-						phrase = phrase + " " + word
-				# ignore 'Golden Globes'
-				if phrase not in gg:
-					properNouns.append(phrase)
-	return properNouns
-
-
-
-
-def sanitizeTweet(text):
-	# remove rewteet
-	cleanTweet = re.sub("RT ", "", text)
-
-	# remove links
-	if re.match(".*http.*(?= )", cleanTweet):
-		cleanTweet = re.sub("http.* ","",cleanTweet)
-	else:
-		cleanTweet = re.sub("http.*","",cleanTweet)
-
-	# remove @ and #
-	symbolsStopList = ["@", "#", "\"", "!", "=", "\.", "\(", "\)", "Golden Globes"]
-	for symbol in symbolsStopList:
-		cleanTweet = re.sub("%s" % symbol, "", cleanTweet)
-
-	# remove source
-	cleanTweet = re.sub("HuffingtonPost", "", cleanTweet)
-	cleanTweet = re.sub("GoldenGlobes", "", cleanTweet)
-
-	return cleanTweet
-
-def sanitizeAwardName(text):
-	cleanAward = text
-	for stopWord in awardNameStopList:
-		cleanAward = re.sub("%s " % stopWord, "", cleanAward)
-
-	return cleanAward
-
-
-def sanitizeTweetForPresenters(text):
-	cleanTweet = text
-
-	stopList = ["RT.*:", "@.*", "#.*", "\[.*\]", "\(.*\)"]
-	# stopList += slangStopList
-	for stopWord in stopList:
-		cleanTweet = re.sub("(?i)%s " % stopWord, "", cleanTweet)
-
-	return cleanTweet
-
-def sanitizeTweetForNominees(text):
-	cleanTweet = text
-	words = ["I", "he", "she", "it", "if"]
-	stopList = ["RT.*:","@.*: ","@", "#"]
-	for stopWord in stopList:
-		cleanTweet = re.sub("(?i)%s " % stopWord, "", cleanTweet)
-
-	return cleanTweet
-
-def sanitizeSlang(text):
-	cleanTweet = text
-	stopList = slangStopList
-	for stopWord in stopList:
-		cleanTweet = re.sub("(?i)%s " % stopWord, "", cleanTweet)
-
-	return cleanTweet
 
 def findHosts(twtrs):
 	possibleHosts = {}
@@ -226,7 +144,6 @@ def findNominees(twtrs):
 			print(nominee, sorted_nominees[nominee])
 
 
-
 def findWinners(tweeters, categories):
 	awardResult = {}
 	THRESHOLD = 200
@@ -261,7 +178,112 @@ def findWinners(tweeters, categories):
 
 	sanitizeAwardResult(awardResult)
 
-	return awardResult
+def findBestWorstDress(tweeters):
+	possibleBestDress = []
+	possibleWorstDress = []
+	bestDressPat = re.compile(".*best dress.*",re.IGNORECASE)
+	worstDressPat = re.compile(".*worst dress.*",re.IGNORECASE)
+	pat = ""
+	for twtr in tweeters:
+		for twt in twtr.tweets:
+			properNoun =[]
+			if bestDressPat.match(twt.text):
+				pat = "best"
+			elif worstDressPat.match(twt.text):
+				pat = "worst"
+			else:
+				continue
+			firstHalfOfTweet = re.search("(?i).*(?=%s)" % pat,twt.text)
+			tokenizedText = nltk.wordpunct_tokenize(firstHalfOfTweet.group())
+
+			if tokenizedText:
+				properNoun = extractProperNouns(tokenizedText)
+				for pn in properNoun:
+					if len(pn.split())==2 :
+						if pat == 'best':
+							possibleBestDress.append(pn)
+						else:
+							possibleWorstDress.append(pn)
+
+	bestData = collections.Counter(possibleBestDress)
+	worstData = collections.Counter(possibleWorstDress)
+	print("\n\nList of Best Dressed:\n========================")
+	for host in bestData.most_common()[0:5]:
+		print(host[0])
+	print("\n\nList of Worst Dressed:\n========================")
+	for host in worstData.most_common()[0:5]:
+		print(host[0])
+
+def extractProperNouns(tokenizedText):
+	taggedText = tagger.tag(tokenizedText)
+	grammar = "NP: {<NNP>*}"
+	cp = nltk.RegexpParser(grammar)
+	chunkedText = cp.parse(taggedText)
+
+	properNouns = []
+	for n in chunkedText:
+		if isinstance(n, nltk.tree.Tree):               
+			if n.label() == 'NP':
+				phrase = ""
+				for word, pos in n.leaves():
+					if len(phrase) == 0:
+						phrase = word
+					else:
+						phrase = phrase + " " + word
+				# ignore 'Golden Globes'
+				if phrase not in gg:
+					properNouns.append(phrase)
+	return properNouns
+
+def sanitizeTweet(text):
+	# remove rewteet
+	cleanTweet = re.sub("RT ", "", text)
+
+	# remove links
+	if re.match(".*http.*(?= )", cleanTweet):
+		cleanTweet = re.sub("http.* ","",cleanTweet)
+	else:
+		cleanTweet = re.sub("http.*","",cleanTweet)
+
+	# remove @ and #
+	symbolsStopList = ["@", "#", "\"", "!", "=", "\.", "\(", "\)", "Golden Globes"]
+	for symbol in symbolsStopList:
+		cleanTweet = re.sub("%s" % symbol, "", cleanTweet)
+
+	return cleanTweet
+
+def sanitizeAwardName(text):
+	cleanAward = text
+	for stopWord in awardNameStopList:
+		cleanAward = re.sub("%s " % stopWord, "", cleanAward)
+
+	return cleanAward
+
+def sanitizeTweetForPresenters(text):
+	cleanTweet = text
+
+	stopList = ["RT.*:", "@.*", "#.*", "\[.*\]", "\(.*\)"]
+	for stopWord in stopList:
+		cleanTweet = re.sub("(?i)%s " % stopWord, "", cleanTweet)
+
+	return cleanTweet
+
+def sanitizeTweetForNominees(text):
+	cleanTweet = text
+	words = ["I", "he", "she", "it", "if"]
+	stopList = ["RT.*:","@.*: ","@", "#"]
+	for stopWord in stopList:
+		cleanTweet = re.sub("(?i)%s " % stopWord, "", cleanTweet)
+
+	return cleanTweet
+
+def sanitizeSlang(text):
+	cleanTweet = text
+	stopList = slangStopList
+	for stopWord in stopList:
+		cleanTweet = re.sub("(?i)%s " % stopWord, "", cleanTweet)
+
+	return cleanTweet
 
 def sanitizeAwardResult(awardResult):
 	for a in awardResult:
@@ -270,7 +292,6 @@ def sanitizeAwardResult(awardResult):
 		print("\n\n",a,"\n========================\n",mostCommon[0:5])
 
 def findSimilarCategory(text,awardCategories):
-	# awardCategories = getCategoriesFromFile('Categories.txt')
 	similarities = {}
 	for award in awardCategories:
 		seq = difflib.SequenceMatcher(a=text.lower(), b=award.lower())
@@ -280,27 +301,24 @@ def findSimilarCategory(text,awardCategories):
 	return mostSimilar
 
 def main():
-	jsonFile = 'goldenglobes.json'
 	eventFile = 'event.txt'
 	categoryFile = 'Categories.txt'
-	properNounFile = 'proper_phrases.txt'
-	#tweets = loadJSONFromFile(jsonFile)
 	awardCategories = getCategoriesFromFile(categoryFile)
 	eventObject = getEventObject(eventFile)
 
-	awardResult = {}#  key is the name of the award, value is the actual winner of the award
 	tweeters = eventObject.reporters
-	# i = 1
-	# for twtr in tweeters:
-	# 	print(twtr.userName)
-	# 	i = i-1
-	# 	if i==0:
-	# 		break
-
+	start = time.clock()
+	print("\nNumber 1")
 	findHosts(tweeters)
-	awardResult = findWinners(tweeters, awardCategories)
+	print("\nNumber 2")
+	findWinners(tweeters,awardCategories)
+	print("\nNumber 3")
 	findPresenters(tweeters)
+	print("\nNumber 4")
 	findNominees(tweeters)
-
+	print("\nNumber 5")
+	findBestWorstDress(tweeters)
+	end = time.clock()
+	print("Total time to run is", (end-start))
 
 main()
